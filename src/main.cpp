feee2a037ff99e1da73ff46c8e7c6b935b50f28b
Dave Plummer <dave@daveandnikki.com>
2023-05-06 19:57:47 -0700

    FastLED ring draw

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,29 @@
 #include <Arduino.h>
+#include <FastLED.h>
 
-void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
+#define LED_PIN0 0
+#define NUM_LEDS 32
+
+static CRGB leds[NUM_LEDS];
+
+void setup() 
+{
+      FastLED.addLeds<WS2812B, LED_PIN0, GRB>(leds, NUM_LEDS);
 }
 
 void loop() 
 {
-  static bool bOn = true;
-  digitalWrite(LED_BUILTIN, bOn);
-  bOn = !bOn;
-  delay(125);
+    static byte initialHue = 0;
+
+    CHSV hsv(initialHue, 255, 240);
+    for (int i = 0; i < NUM_LEDS; i++)
+    {
+        CRGB rgb;
+        hsv2rgb_rainbow(hsv, rgb);
+        leds[i] = hsv;
+        hsv.hue += (256/NUM_LEDS);
+    }
+    initialHue += 4;
+    FastLED.show();
+    delay(30);
 }
